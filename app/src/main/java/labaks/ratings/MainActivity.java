8ab480ad6599d8753cdda466c9bbfc40581bc530
labaks <las93@bk.ru>
2017-04-28 15:43:17 +0300

    try to add filtering

--- app/src/main/java/labaks/ratings/MainActivity.java
+++ app/src/main/java/labaks/ratings/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 
@@ -51,7 +54,6 @@ public class MainActivity extends AppCompatActivity {
                 super.populateView(v, model, position);
                 final String itemId = format("item%d", position);
 
-
                 v.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
@@ -67,6 +69,22 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         listItem.setAdapter(itemAdapter);
+
+        EditText inputSearch = (EditText) findViewById(R.id.etSearch);
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                MainActivity.this.itemAdapter.getFilter().filter(charSequence);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
     }
 
     @Override
